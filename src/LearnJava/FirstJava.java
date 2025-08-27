e276bbea1f2b9a58f382bc9c5e1c1d4f8217baf0
7404141-22 <lokeshsingh0666@gmail.com>
2025-08-27 11:00:06 +0530

    firstcode

---
+++ src/LearnJava/FirstJava.java
@@ -0,0 +1,9 @@
+package LearnJava;
+
+import com.sun.tools.javac.Main;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("first hello");
+    }
+}
