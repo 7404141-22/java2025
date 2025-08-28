34c5355169f64d3fc78e4610c58585787a089d4b
7404141-22 <lokeshsingh0666@gmail.com>
2025-08-28 15:09:55 +0530

    firstcode

---
+++ src/LearnJava/SecJava.java
@@ -0,0 +1,9 @@
+package LearnJava;
+
+public class SecJava {
+    public static void main(String[] args) {
+        System.out.println("comment");
+//        System.out.println();
+//        System.out.println();
+    }
+}
