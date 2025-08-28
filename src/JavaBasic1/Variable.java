aee85cc71604ea392022163a7c6b2951289e2e40
7404141-22 <lokeshsingh0666@gmail.com>
2025-08-28 16:56:26 +0530

    variablenew

---
+++ src/JavaBasic1/Variable.java
@@ -0,0 +1,10 @@
+package JavaBasic1;
+
+public class Variable {
+    public static void main(String[] args) {
+        int age =76;
+        age = age+1;
+        age =76;
+        System.out.println(age);
+    }
+}
