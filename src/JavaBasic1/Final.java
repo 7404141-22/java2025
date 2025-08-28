ec2dcfa0fc1849b962d1ba4e8fd6c31481fa01be
7404141-22 <lokeshsingh0666@gmail.com>
2025-08-28 17:51:04 +0530

    all1

---
+++ src/JavaBasic1/Final.java
@@ -0,0 +1,9 @@
+package JavaBasic1;
+
+public class Final {
+    public static void main(String[] args) {
+        final int a =10;
+        final  float PI = 3.14f;
+        System.out.println(a);
+    }
+}
---
+++ src/JavaBasic1/PrintF.java
@@ -0,0 +1,8 @@
+package JavaBasic1;
+
+public class PrintF {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.printf("you variable is %d",a);
+    }
+}
